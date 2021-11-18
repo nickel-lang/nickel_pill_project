bdebe227b68eb129a4d6a68670e3b3c4b74530ee
Litchi Pi <litchi.pi@protonmail.com>
2021-11-18 17:22:20 +0100

    Pill #2: Simple manual configuration
    Signed-off-by: Litchi Pi <litchi.pi@protonmail.com>

--- app/app.py
+++ app/app.py
@@ -1,3 +1,19 @@
 #! /usr/bin/env python3
 
-print("Hello, World!")
+import json
+from flask import Flask
+
+app = Flask(__name__)
+with open("./config.json", "r") as f:
+    configuration = json.load(f)
+
+@app.route("/")
+def hello():
+    return "Hello, World!"
+
+def run(cfg):
+    app.run(host=cfg["host"], port=cfg["port"])
+
+
+if __name__ == "__main__":
+    run(configuration)
